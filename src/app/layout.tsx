01642b81128b7cdbd0c614b15c09a5adca845187
Codewith-AG <Codewith-AG@users.noreply.github.com>
2026-08-03 22:49:26 +0530

    Configure custom favicon and update gitignore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,9 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "Credentials Vault",
   description: "High-End Credentials Manager",
+  icons: {
+    icon: "/logo_2.png",
+  },
 };
 
 export default function RootLayout({
